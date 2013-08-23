bf829c7cc5b5b6a7fcf2a942e2045734a0c54bdc
Joyce Echessa <jokhessa@yahoo.com>
2013-08-23 13:11:52 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fafa::Application.config.secret_token = '984f97d093fb5f024a7ed46d4a2620f355d003638ec7e9238646dcdb96161955fd7ca3a43ec4ca9d762ea5b6bc3e7bbb2497e73bfc20c6c47e5196c1ba5c1168'
